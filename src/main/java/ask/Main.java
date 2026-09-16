2ea87596c09acab69a0d757bf481c95d03d25801
jominhye <jominhye@partner.gongyoung.kr>
2026-09-16 17:57:54 +0900

    teest

--- src/main/java/ask/Main.java
+++ src/main/java/ask/Main.java
@@ -1,10 +1,33 @@
 package ask;
 
+import com.sun.tools.javac.util.StringUtils;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println(11);
+
+        /*try {
+            String s1 = URLEncoder.encode("상단 롤링 대배너", "UTF-8").replace("+", "%20");
+            String s2 = URLEncoder.encode("메인 퀵메뉴(세로형)", "UTF-8").replace("+", "%20").replace("%28", "(").replace("%29", ")");
+			String s3 = URLEncoder.encode("메인 공영라방_더보기", "UTF-8").replace("+", "%20");
+
+            String s4 = URLEncoder.encode("라방 라이브", "UTF-8").replace("+", "%20");
+            String s5 = URLEncoder.encode("라방 VOD", "UTF-8").replace("+", "%20");
+			System.out.println(s4);
+			System.out.println(s5);
+        } catch (UnsupportedEncodingException e) {
+            throw new RuntimeException(e);
+        }*/
+        
+		
+		
+		
+		
 	}
 
 }
